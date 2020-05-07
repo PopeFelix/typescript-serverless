818aab94bd7c23030a54d1565841f7a1b431c0c8
Kit Peters <christopher.peters@cbsinteractive.com>
2020-05-07 14:54:31 -0500

    Add my personalized config

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,17 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const path = require('path');
-const slsw = require('serverless-webpack');
-const nodeExternals = require('webpack-node-externals');
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const path = require('path')
+const slsw = require('serverless-webpack')
+const nodeExternals = require('webpack-node-externals')
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+/* eslint-enable @typescript-eslint/no-var-requires */
 
 module.exports = {
   context: __dirname,
   mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
   entry: slsw.lib.entries,
-  devtool: slsw.lib.webpack.isLocal ? 'cheap-module-eval-source-map' : 'source-map',
+  devtool: slsw.lib.webpack.isLocal
+    ? 'cheap-module-eval-source-map'
+    : 'source-map',
   resolve: {
     extensions: ['.mjs', '.json', '.ts'],
     symlinks: false,
@@ -54,4 +57,4 @@ module.exports = {
       },
     }),
   ],
-};
+}
